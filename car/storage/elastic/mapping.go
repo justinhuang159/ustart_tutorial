20310c1c01c39f5108e75a608acfe352be81747e
Justin Huang <justinhuang159@gmail.com>
2019-06-06 14:28:22 -0400

    added mappings to car

---
+++ car/storage/elastic/mapping.go
@@ -0,0 +1,100 @@
+package elasticstore
+
+func mapping(indexName string) string {
+
+	return `{
+		"settings": {
+			"analysis": {
+				"analyzer": {
+					"my_analyzer": {
+						"type": "custom",
+						"filter": [
+							"lowercase"
+						],
+						"tokenizer": "whitespace"
+					}
+				}
+			}
+		},
+		"mappings":{
+			"properties":{
+				"Make":{
+					"type": "text",
+					"analyzer":"my_analyzer",
+					"fields":{
+						"raw":{
+							"type":"keyword"
+						}
+					}
+				},
+				"Model":{
+					"type": "text",
+					"analyzer":"my_analyzer",
+					"fields":{
+						"raw":{
+							"type":"keyword"
+						}
+					}
+				},
+				"Year":{
+					"type": "text",
+					"analyzer":"my_analyzer",
+					"fields":{
+						"raw":{
+							"type":"keyword"
+						}
+					}
+				},
+				"Color":{
+					"type": "text",
+					"analyzer":"my_analyzer",
+					"fields":{
+						"raw":{
+							"type":"keyword"
+						}
+					}
+				},
+				"Class":{
+					"type": "text",
+					"analyzer":"my_analyzer",
+					"fields":{
+						"raw":{
+							"type":"keyword"
+						}
+					}
+				},
+				"Available":{
+					"type": "boolean",
+					"analyzer":"my_analyzer",
+					"fields":{
+						"raw":{
+							"type":"keyword"
+						}
+					}
+				},
+				"AccidentHistory":{
+					"type": "nested",
+					"properties":{
+						"Date":{
+							"type": "text",
+							"analyzer":"my_analyzer",
+							"fields":{
+								"raw":{
+									"type":"keyword"
+								}
+							}
+						},
+						"Details":{
+							"type": "text",
+							"analyzer":"my_analyzer",
+							"fields":{
+								"raw":{
+									"type":"keyword"
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}`
